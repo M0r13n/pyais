9d5d5907825190cda1d0dfc09822a8ea6e2bd246
M0r13n <31622033+M0r13n@users.noreply.github.com>
2019-10-05 13:52:48 +0200

    implement more decoders and add a checksum calculator

--- pyais.py
+++ pyais.py
@@ -9,10 +9,23 @@ from math import ceil
 from bitstring import BitArray
 
 
+def catch_error(*exceptions):
+    def checking(f):
+        def checked(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except exceptions as ex:
+                print(f"\x1b[31mCould not parse AIS packet, because: {str(ex)}!\x1b[0m")
+
+        return checked
+
+    return checking
+
+
 def decode_ascii6(data):
     """
     Decode AIS_ASCII_6 encoded data and convert it into binary.
-    :param data: ASI_ASCII_& encoded data
+    :param data: ASI_ASCII_6 encoded data
     :return: a binary string of 0's and 1's, e.g. 011100 011111 100001
     """
     binary_string = ''
@@ -41,6 +54,26 @@ def split_str(string, chunk_size=6):
     return lst
 
 
+def ascii6(data, ignore_tailing_fillers=True):
+    """
+    Decode bit sequence into ASCII6.
+    :param data: ASI_ASCII_6 encoded data
+    :return: ASCII String
+    """
+    string = ""
+    for c in split_str(data):
+        c = int(c, 2)
+        if c < 32:
+            c += 64
+        c = chr(c)
+
+        if ignore_tailing_fillers and c == '@':
+            return string
+        string += c
+
+    return string
+
+
 def signed(bit_vector):
     """
     convert bit sequence to signed integer
@@ -51,45 +84,126 @@ def signed(bit_vector):
     return b.int
 
 
+def to_int(bit_string, base=2):
+    """
+    Convert a sequence of bits to int while ignoring empty strings
+    :param bit_string: sequence of zeros and ones
+    :param base: the base
+    :return: a integer or None if no valid bit_string was provided
+    """
+    if bit_string:
+        return int(bit_string, base)
+    return 0
+
+
+def checksum(msg):
+    """
+    Compute the checksum of a given message
+    :param msg: message
+    :return: hex
+    """
+    calc = False
+    c_sum = 0
+    for c in msg:
+        if c == '$' or c == '!':
+            calc = True
+            continue
+
+        if c == '*':
+            break
+
+        if calc:
+            c_sum ^= ord(c)
+    return c_sum
+
+
 def decode_msg_1(bit_vector):
     """
     AIS Vessel position report using SOTDMA (Self-Organizing Time Division Multiple Access)
-    Src: http://www.bosunsmate.org/ais/message1.php
+    Src: https://gpsd.gitlab.io/gpsd/AIVDM.html#_types_1_2_and_3_position_report_class_a
     """
+    if (len(bit_vector)) != 168:
+        print(bit_vector)
     return {
-        'msg_type': int(bit_vector[0:6], 2),
-        'repeat_indicator': int(bit_vector[6:8], 2),
-        'user_id': int(bit_vector[8:38], 2),
-        'nav_status': int(bit_vector[38:42], 2),
-        'rot': int(bit_vector[42:50], 2),
-        'sog': int(bit_vector[50:60], 2),
-        'pos_accuracy': int(bit_vector[60:61], 2),
-        'longitude': signed(bit_vector[61:89]) / 600000.0,
-        'latitude': signed(bit_vector[89:116]) / 600000.0,
-        'cog': int(bit_vector[116:128], 2),
-        'hdg': int(bit_vector[128:137], 2),
-        'utc': int(bit_vector[137:143], 2),
-        'raim': int(bit_vector[148:149], 2),
-        'sotdma_sync_state': int(bit_vector[149:151], 2),
-        'sotdma_slot_timeout': int(bit_vector[151:154], 2),
-        'sotdma_sync_offset': int(bit_vector[154:168], 2),
+        'type': to_int(bit_vector[0:6], 2),
+        'repeat': to_int(bit_vector[6:8], 2),
+        'mmsi': to_int(bit_vector[8:38], 2),
+        'status': to_int(bit_vector[38:42], 2),
+        'turn': signed(bit_vector[42:50]),
+        'speed': to_int(bit_vector[50:60], 2),
+        'accuracy': to_int(bit_vector[60], 2),
+        'lon': signed(bit_vector[61:89]) / 600000.0,
+        'lat': signed(bit_vector[89:116]) / 600000.0,
+        'course': to_int(bit_vector[116:128], 2) * 0.1,
+        'heading': to_int(bit_vector[128:137], 2),
+        'second': to_int(bit_vector[137:143], 2),
+        'maneuver': to_int(bit_vector[143:145], 2),
+        'raim': bool(to_int(bit_vector[148], 2)),
+        'radio': to_int(bit_vector[149::], 2)
     }
 
 
 def decode_msg_2(bit_vector):
-    pass
+    """AIS Vessel position report using SOTDMA (Self-Organizing Time Division Multiple Access)
+    Src: https://gpsd.gitlab.io/gpsd/AIVDM.html#_types_1_2_and_3_position_report_class_a
+    """
+    return decode_msg_1(bit_vector)
 
 
 def decode_msg_3(bit_vector):
-    pass
+    """
+    AIS Vessel position report using ITDMA (Incremental Time Division Multiple Access)
+    Src: https://gpsd.gitlab.io/gpsd/AIVDM.html#_types_1_2_and_3_position_report_class_a
+    """
+    return decode_msg_1(bit_vector)
 
 
 def decode_msg_4(bit_vector):
-    pass
+    """
+    AIS Vessel position report using SOTDMA (Self-Organizing Time Division Multiple Access)
+    Src: https://gpsd.gitlab.io/gpsd/AIVDM.html#_type_4_base_station_report
+    """
+    return {
+        'type': to_int(bit_vector[0:6], 2),
+        'repeat': to_int(bit_vector[6:8], 2),
+        'mmsi': to_int(bit_vector[8:38], 2),
+        'year': to_int(bit_vector[38:52], 2),
+        'month': to_int(bit_vector[52:56]),
+        'day': to_int(bit_vector[56:61], 2),
+        'hour': to_int(bit_vector[61:66], 2),
+        'minute': to_int(bit_vector[66:72], 2),
+        'second': to_int(bit_vector[72:78], 2),
+        'accuracy': bool(to_int(bit_vector[78], 2)),
+        'lon': signed(bit_vector[66:72]) / 600000.0,
+        'lat': signed(bit_vector[66:72]) / 600000.0,
+        'epfd': to_int(bit_vector[134:138], 2),
+        'raim': bool(to_int(bit_vector[148], 2)),
+        'radio': to_int(bit_vector[148::], 2)
+    }
 
 
 def decode_msg_5(bit_vector):
-    pass
+    return {
+        'type': to_int(bit_vector[0:6], 2),
+        'repeat': to_int(bit_vector[6:8], 2),
+        'mmsi': to_int(bit_vector[8:38], 2),
+        'ais_version': to_int(bit_vector[38:40], 2),
+        'imo': to_int(bit_vector[40:70], 2),
+        'callsign': ascii6(bit_vector[70:112]),
+        'shipname': ascii6(bit_vector[112:232]),
+        'shiptype': to_int(bit_vector[66:72], 2),
+        'to_bow': to_int(bit_vector[240:249], 2),
+        'to_stern': to_int(bit_vector[249:258], 2),
+        'to_port': to_int(bit_vector[258:264], 2),
+        'to_starboard': to_int(bit_vector[264:270], 2),
+        'epfd': to_int(bit_vector[270:274], 2),
+        'month': to_int(bit_vector[274:278], 2),
+        'day': to_int(bit_vector[278:283], 2),
+        'hour': to_int(bit_vector[283:288], 2),
+        'minute': to_int(bit_vector[288:294], 2),
+        'draught': to_int(bit_vector[294:302], 2) / 10.0,
+        'destination': ascii6(bit_vector[302::])
+    }
 
 
 def decode_msg_6(bit_vector):
@@ -141,7 +255,31 @@ def decode_msg_17(bit_vector):
 
 
 def decode_msg_18(bit_vector):
-    pass
+    """
+    Standard Class B CS Position Report
+    Src: https://gpsd.gitlab.io/gpsd/AIVDM.html#_type_18_standard_class_b_cs_position_report
+    """
+    return {
+        'type': to_int(bit_vector[0:6], 2),
+        'repeat': to_int(bit_vector[6:8], 2),
+        'mmsi': to_int(bit_vector[8:38], 2),
+        'speed': to_int(bit_vector[46:55], 2),
+        'accuracy': bool(to_int(bit_vector[55], 2)),
+        'lon': signed(bit_vector[56:85]) / 600000.0,
+        'lat': signed(bit_vector[85:112]) / 600000.0,
+        'course': to_int(bit_vector[112:124], 2) * 0.1,
+        'heading': to_int(bit_vector[124:133], 2),
+        'second': to_int(bit_vector[133:139], 2),
+        'regional': to_int(bit_vector[139:141], 2),
+        'cs': bool(to_int(bit_vector[141])),
+        'display': bool(to_int(bit_vector[142])),
+        'dsc': bool(to_int(bit_vector[143])),
+        'band': bool(to_int(bit_vector[144])),
+        'msg22': bool(to_int(bit_vector[145])),
+        'assigned': bool(to_int(bit_vector[146])),
+        'raim': bool(to_int(bit_vector[147])),
+        'radio': to_int(bit_vector[148::]),
+    }
 
 
 def decode_msg_19(bit_vector):
@@ -210,9 +348,13 @@ def main():
 
     for msg in ais_stream():
         if msg and msg[0] == "!":
-            m_typ, n_sentences, sentence_num, seq_id, channel, data, checksum = msg.split(',')
+            m_typ, n_sentences, sentence_num, seq_id, channel, data, chcksum = msg.split(',')
             decoded_data = decode_ascii6(data)
             msg_type = int(decoded_data[0:6], 2)
+
+            if checksum(msg) != int("0x" + chcksum[2::], 16):
+                print(f"\x1b[31mInvalid Checksum dropping packet!\x1b[0m")
+                continue
 
             if 0 < msg_type < 25:
                 print(DECODE_MSG[msg_type](decoded_data))
