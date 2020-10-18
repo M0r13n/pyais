2a930cf041454a7cd5d4136b717b94f96f928ae0
Leon Morten Richter <github@leonmortenrichter.de>
2020-10-18 14:50:24 +0200

    bump version to 1.1.2

--- pyais/__init__.py
+++ pyais/__init__.py
@@ -2,7 +2,7 @@ from pyais.messages import NMEAMessage, AISMessage
 from pyais.stream import TCPStream, FileReaderStream
 
 __license__ = 'MIT'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 __all__ = (
     'NMEAMessage',
