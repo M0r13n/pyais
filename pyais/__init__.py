a2ee71cf7c71a7bf8538ac76a4a171ef2c06b743
Leon Morten Richter <github@leonmortenrichter.de>
2022-02-06 16:13:59 +0100

    Add a deprecation warning about v2

--- pyais/__init__.py
+++ pyais/__init__.py
@@ -1,10 +1,12 @@
+import warnings
+
 from pyais.messages import NMEAMessage, AISMessage
 from pyais.stream import TCPStream, FileReaderStream, IterMessages
 from pyais.decode import decode_msg
 from pyais.encode import encode_dict, encode_payload
 
 __license__ = 'MIT'
-__version__ = '1.7.0'
+__version__ = '1.7.1'
 
 __all__ = (
     'decode_msg',
@@ -16,3 +18,9 @@ __all__ = (
     'IterMessages',
     'FileReaderStream'
 )
+
+warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+warnings.warn(
+    "Version 2.0.0 will introduce breaking changes. Either pin the version to 1.7.1 or switch to pyais==2.0.0-alpha to upgrade to the newest version.",
+    category=DeprecationWarning)
+warnings.simplefilter('default', DeprecationWarning)  # reset filter
