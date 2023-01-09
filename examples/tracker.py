f722e49f12c1b6fbeb67c39720c241e440a1ac89
Leon Morten Richter <github@leonmortenrichter.de>
2023-01-09 15:05:41 +0100

    Ais tracks (#102)
    * add AISTracker and AISTrack classes
    
    * adds l_latest_tracks and get_track
    
    * simply tracking logic
    
    * add missing tests and docs
    
    * remove slots from dataclass, because it is not supported in older versions of Python

---
+++ examples/tracker.py
@@ -0,0 +1,17 @@
+import pathlib
+
+from pyais import AISTracker
+from pyais.stream import FileReaderStream
+
+filename = pathlib.Path(__file__).parent.joinpath('sample.ais')
+
+with AISTracker() as tracker:
+    for msg in FileReaderStream(str(filename)):
+        tracker.update(msg)
+        latest_tracks = tracker.n_latest_tracks(10)
+
+# Get the latest 10 tracks
+print('latest 10 tracks', ','.join(str(t.mmsi) for t in latest_tracks))
+
+# Get a specific track
+print(tracker.get_track(249191000))
--- pyais/__init__.py
+++ pyais/__init__.py
@@ -1,10 +1,11 @@
-from pyais.messages import NMEAMessage, ANY_MESSAGE
+from pyais.messages import NMEAMessage, ANY_MESSAGE, AISSentence
 from pyais.stream import TCPConnection, FileReaderStream, IterMessages
 from pyais.encode import encode_dict, encode_msg, ais_to_nmea_0183
 from pyais.decode import decode
+from pyais.tracker import AISTracker, AISTrack
 
 __license__ = 'MIT'
-__version__ = '2.4.0'
+__version__ = '2.5.0'
 __author__ = 'Leon Morten Richter'
 
 __all__ = (
@@ -12,9 +13,12 @@ __all__ = (
     'encode_msg',
     'ais_to_nmea_0183',
     'NMEAMessage',
+    'AISSentence',
     'ANY_MESSAGE',
     'TCPConnection',
     'IterMessages',
     'FileReaderStream',
     'decode',
+    'AISTracker',
+    'AISTrack',
 )
