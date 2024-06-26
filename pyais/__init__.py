725a63f68071df55c101ee96bb2f0b1b14aa052e
Leon Morten Richter <github@leonmortenrichter.de>
2024-06-26 11:40:48 +0200

    chore: bumps versions & updates changelog

--- pyais/__init__.py
+++ pyais/__init__.py
@@ -5,7 +5,7 @@ from pyais.decode import decode
 from pyais.tracker import AISTracker, AISTrack
 
 __license__ = 'MIT'
-__version__ = '2.6.5'
+__version__ = '2.6.6'
 __author__ = 'Leon Morten Richter'
 
 __all__ = (
