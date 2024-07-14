e7ea39b8a9776d0674ff97f2774338587b3827b7
Leon Morten Richter <github@leonmortenrichter.de>
2024-07-14 10:46:12 +0200

    chore: removes unsued statement

--- examples/raw_payload.py
+++ examples/raw_payload.py
@@ -4,7 +4,6 @@ from pyais.messages import MSG_CLASS
 from pyais.util import get_int
 
 payload = b'\x20\x6f\xab\x71\xdf\x80\x32\xe3\x6e\x78\x3d\xda\xa7\x49\x65\xf4\xca\x9f\x6b\xe7\x07'
-fill_bits = 0
 
 bit_array = bitarray.bitarray()
 bit_array.frombytes(payload)
