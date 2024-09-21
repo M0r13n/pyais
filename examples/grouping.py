523e709c64c0ada292fa03b65ef948daf0fb920a
Leon Morten Richter <github@leonmortenrichter.de>
2024-09-21 13:47:10 +0200

    chore: docs

--- examples/grouping.py
+++ examples/grouping.py
@@ -1,15 +1,14 @@
-import queue
 from pyais.stream import FileReaderStream, TagBlockQueue
 import pathlib
 
 filename = pathlib.Path(__file__).parent.joinpath('sample.ais')
 
+# To track NMEA 4.10 tag block groups a queue is required.
+# This queue buffers NMEA sentences belonging until the group is complete.
+# NOTE: get_nowait will return NMEA sentences - NOT AIS sentences!
+tbq = TagBlockQueue()
 
-with FileReaderStream(str(filename), tbq=TagBlockQueue()) as stream:
-    tbq = stream.tbq
-
+with FileReaderStream(str(filename), tbq=tbq) as stream:
     for msg in stream:
-        try:
+        while not tbq.empty():
             print(tbq.get_nowait())
-        except queue.Empty:
-            pass
