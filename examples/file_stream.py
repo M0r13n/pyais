3eceea585771e9e7461c24c270c69d07a66370b4
Leon Morten Richter <github@leonmortenrichter.de>
2025-03-22 12:51:25 +0100

    chore: extent example file_stream.py

--- examples/file_stream.py
+++ examples/file_stream.py
@@ -14,6 +14,24 @@ from pyais.stream import FileReaderStream
 filename = pathlib.Path(__file__).parent.joinpath('sample.ais')
 
 with FileReaderStream(str(filename)) as stream:
-    for msg in stream:
-        decoded = msg.decode()
-        print(decoded)
+    for nmea_msg in stream:
+        # Assemble the NMEA message layer and decode AIS payload
+        ais_data = nmea_msg.decode()
+
+        # multi-line NMEA sentences
+        print(f"NMEA message is multi part: {'yes' if nmea_msg.is_multi else 'no'} ")
+
+        # tag blocks
+        if nmea_msg.tag_block:
+            nmea_msg.tag_block.init()
+            tb_dict = nmea_msg.tag_block.asdict()
+            print(f"source_station: {tb_dict.get('source_station', 'n.a.')}")
+
+        #  gatehouse wrappers
+        if nmea_msg.wrapper_msg is not None:  # <= optional gatehouse wrapper
+            print('Gatehouse')
+            print('Country', nmea_msg.wrapper_msg.country)
+            print('Online', nmea_msg.wrapper_msg.online_data)
+            print('PSS', nmea_msg.wrapper_msg.pss)
+            print('Region', nmea_msg.wrapper_msg.region)
+            print('Timestamp', nmea_msg.wrapper_msg.timestamp)
