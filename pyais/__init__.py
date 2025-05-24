677f8de093a206c2a5792825b1cdbb0206bffb57
Leon Morten Richter <github@leonmortenrichter.de>
2025-05-24 13:55:26 +0200

    chore: prepare new release

--- pyais/__init__.py
+++ pyais/__init__.py
@@ -5,7 +5,7 @@ from pyais.decode import decode
 from pyais.tracker import AISTracker, AISTrack
 
 __license__ = 'MIT'
-__version__ = '2.9.2'
+__version__ = '2.9.3'
 __author__ = 'Leon Morten Richter'
 
 __all__ = (
